9a95129604e39aa3507081783ef4cc7bb5435bac
Samuel Holanda <samueloliveirad337@gmail.com>
2025-11-06 17:02:43 -0300

    create a test on Animal class and add a test instance on a Main class.

---
+++ src/crud/model/Animal.java
@@ -0,0 +1,42 @@
+package crud.model;
+
+import java.sql.SQLOutput;
+
+public class Animal {
+    private String name;
+    private int age;
+    private String breed;
+
+    public Animal(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    //method test
+    public void showInfo() {
+        System.out.println("Your pet:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Breed: " + breed);
+    }
+}
--- src/crud/view/Main.java
+++ src/crud/view/Main.java
@@ -1,7 +1,11 @@
 package crud.view;
 
+import crud.model.Animal;
+
 public class Main {
     public static void main(String[] args) {
+        Animal novoAnimal = new Animal("Josias", 21, "Dog");
+        novoAnimal.showInfo();
         System.out.println("Test");
     }
 }
