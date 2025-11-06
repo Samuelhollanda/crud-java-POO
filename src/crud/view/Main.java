6f71ab3263b610df5eb553cecc12a05d7371a4f8
Samuel Holanda <samueloliveirad337@gmail.com>
2025-11-06 17:10:37 -0300

    add a simple sintaxe reference a switch case

--- src/crud/view/Main.java
+++ src/crud/view/Main.java
@@ -7,5 +7,16 @@ public class Main {
         Animal novoAnimal = new Animal("Josias", 21, "Dog");
         novoAnimal.showInfo();
         System.out.println("Test");
+        int op = 2;
+
+
+        switch (op) {
+            case 1:
+                System.out.println("Hello Word!!");
+                break;
+            case 2:
+                System.out.println("Word Hello");
+                break;
+        }
     }
 }
